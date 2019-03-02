a36f1ef668086d4c0d582241edb58599aea6d032
Shannon Clarke <shannonajclarke@gmail.com>
2019-03-02 11:31:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Deploying NodeJS Docker Container to Heroku
+
+Instructions for deploying a docker container with a NodeJS setup to Heroku
+
+## Quick start
+
+Complete the following steps in command line:
+
+- Clone this git repo locally
+- In the cloned repo, install dependencies `npm install`
+- Run locally `npm start`
+
---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// We're using the express library we pulled in using npm:
+const express = require('express');
+
+// Constants (note this will get weird later)
+const PORT = 5000;
+
+// We start an express app:
+const app = express();
+
+// At the root, create a `send` element for the `res`ponse.  There is no 
+// option to pass in a `req`uest at this time.
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello world</h1>\nAnd a special hello to Simon!');
+});
+
+// Then listen to the port to see if there are any `get` calls to the root (with the response defined above):
+app.listen(process.env.PORT || PORT);
+console.log('Running on http://localhost:' + PORT);
